b15fad643a6aa0ca7ede2306e7580fc08f65c17d
Stephane Nicoll <snicoll@vmware.com>
2022-11-22 08:39:17 +0100

    Allow Gradle DSL to be chosen for any language
    This commit updates the UI to expose a Gradle and a Gradle Kotlin option
    so that users can pick their favorite DSL. The automatic fallback to
    the Kotlin DSL when the Kotlin language is selected has been removed
    as a result.
    
    Closes gh-956

--- start-site/src/test/java/io/spring/start/site/extension/dependency/springcloud/SpringCloudContractGradleBuildCustomizerTests.java
+++ start-site/src/test/java/io/spring/start/site/extension/dependency/springcloud/SpringCloudContractGradleBuildCustomizerTests.java
@@ -61,8 +61,8 @@ class SpringCloudContractGradleBuildCustomizerTests extends AbstractExtensionTes
 	@Test
 	void springCloudContractVerifierPluginWithKotlinDslAndTestModeSetWhenWebFluxIsPresent() {
 		ProjectRequest projectRequest = createProjectRequest("cloud-contract-verifier", "webflux");
-		projectRequest.setLanguage(KotlinLanguage.ID);
-		assertThat(gradleBuild(projectRequest))
+		projectRequest.setType("gradle-project-kotlin");
+		assertThat(generateProject(projectRequest)).textFile("build.gradle.kts")
 				.contains("import org.springframework.cloud.contract.verifier.config.TestMode")
 				.containsSubsequence("contracts {", "testMode.set(TestMode.WEBTESTCLIENT)");
 	}
